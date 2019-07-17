289b554d4f62947d4163c08b26cc8a1941f39cf0
Henrique Morato <henrique.morato@campuscode.com.br>
2019-07-16 22:09:48 -0300

    create e edit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,33 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(params.require(:recipe).permit(recipe_params))
+    @recipe.save
+    redirect_to @recipe
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+    recipe.update(params.require(:recipe).permit(recipe_params))
+    redirect_to @recipe
+  end
+
+
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+
+  def recipe_params
+    [:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method ]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'recipes#index'
 
-  resources :recipes, only: %i[index show]
+
+  resources :recipes, only: %i[index show new create edit update]
 end
